84e4a8f50b052ec43e5c6170e9e910fe611c5c54
MUKESH M <76618089+Ajay-Mukesh@users.noreply.github.com>
2021-10-20 19:21:52 +0530

    Add files via upload

---
+++ Mathrithms.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Mathrithms
+{
+pulblic static void main(String args[]){
+
+int arr[] = {83, 1, 45, 95, 45, 52, 11, 47, 0 ,45, 67, 82};
+Arrays.sort(arr);
+System.out.println("Sortted Array's are ",Arrays.toString(arr));
+}
+}
